5971e6a151897ded4d138451cff88877ea86ddd0
JoKalk <j.kalkhof@hotmail.de>
2022-09-22 11:08:54 +0200

    New Upsample Approach

--- train_NCA_refine_from3D_prostate.py
+++ train_NCA_refine_from3D_prostate.py
@@ -16,7 +16,7 @@ from IPython.display import clear_output
 #from lib.utils_vis import SamplePool, to_alpha, to_rgb, get_living_mask, make_seed, make_circle_masks
 from src.losses.LossFunctions import DiceLoss, DiceBCELoss
 from src.utils.Experiment import Experiment, DataSplit
-from src.agents.Agent_NCA_refineResults import Agent_RefineResults
+from src.agents.Agent_NCA_refineResultsUpsample import Agent_RefineResults_Upsample
 from src.models.Model_BasicNCA import BasicNCA
 from src.models.Model_LearntPerceiveNCA import LearntPerceiveNCA
 from src.utils.collate_variable_size import collate_variable_size
@@ -35,7 +35,7 @@ def main():
         'img_path': r"M:\MasterThesis\Datasets\Prostate\preprocessed_dataset_train\imagesTr",
         'label_path': r"M:\MasterThesis\Datasets\Prostate\preprocessed_dataset_train\labelsTr",
         'data_type': '.nii.gz', # .nii.gz, .jpg
-        'model_path': r'M:\Models\TestNCA_Prostate_refine_full7',
+        'model_path': r'M:\Models\TestNCA_Prostate_refine_full12',
         'device':"cuda:0",
         'n_epoch': 200,
         # Learning rate
@@ -44,7 +44,7 @@ def main():
         'betas': (0.5, 0.5),
         'inference_steps': [64],
         # Training config
-        'save_interval': 5,
+        'save_interval': 10,
         'evaluate_interval': 10,
         # Model config
         'channel_n': 16,        # Number of CA state channels
@@ -59,10 +59,10 @@ def main():
         'output_channels': 3,
         'stacked_models': 2,
         'scaling_factor': 4, # each axis
-        'train_model': 0,
+        'train_model': 1,
         # Data
         'input_size': [(64, 64), (128, 128)],
-        'data_split': [0.6, 0, 0.4], 
+        'data_split': [0.7, 0, 0.3], 
         'pool_chance': 0.5,
         'Persistence': False,
     },
@@ -76,11 +76,11 @@ def main():
     device = torch.device(config[0]['device'])
 
     # Define all model levels
-    ca_lvl0 = LearntPerceiveNCA(config[0]['channel_n'], config[0]['cell_fire_rate'], device, hidden_size=128).to(device)
-    ca_lvl1 = LearntPerceiveNCA(config[0]['channel_n'], config[0]['cell_fire_rate'], device, hidden_size=128).to(device)
+    ca_lvl0 = LearntPerceiveNCA(config[0]['channel_n'], config[0]['cell_fire_rate'], device, hidden_size=32).to(device)
+    ca_lvl1 = LearntPerceiveNCA(config[0]['channel_n'], config[0]['cell_fire_rate'], device, hidden_size=32).to(device)
     ca = [ca_lvl0, ca_lvl1]
 
-    agent = Agent_RefineResults(ca)
+    agent = Agent_RefineResults_Upsample(ca)
     exp = Experiment(config, dataset, ca, agent)
     exp.set_model_state('train')
     data_loader = torch.utils.data.DataLoader(dataset, shuffle=True, batch_size=exp.get_from_config('batch_size')) #, collate_fn=collate_variable_size
