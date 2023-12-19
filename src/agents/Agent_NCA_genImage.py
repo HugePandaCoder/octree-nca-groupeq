Fixed backprop trick

src/agents/Agent_NCA_genImage.py
@@ -64,6 +64,8 @@ class Agent_NCA_genImage(Agent_NCA_gen):
                 loss.backward()
                 self.optimizer_backpropTrick.step()
 
+
+            print(self.model.list_backpropTrick[0].weight.squeeze().detach())
             for i, v in enumerate(vec):
             # Add optimizer trick here
                 
@@ -73,6 +75,7 @@ class Agent_NCA_genImage(Agent_NCA_gen):
                     vec_loss = loss_f(outputs[i, ...], targets[i, ...])
                     vec_loss.backward(retain_graph=True)
                     self.optimizer_backpropTrick.step()
+                    print("NOT STANDARD")
                 
                 
                 # MOVE loss to vector
@@ -81,7 +84,8 @@ class Agent_NCA_genImage(Agent_NCA_gen):
 
                 
                 #vec_loss = F.mse_loss(outputs[i, ...], targets[i, ...])
-                v = v.to(self.device) - ((1.0-self.model.list_backpropTrick[i].weight.squeeze()).detach())*24000*100#*vec_loss
+                
+                v = v.to(self.device) - ((1.0-self.model.list_backpropTrick[i].weight.squeeze()).detach())*24000#*10000000#*vec_loss
                 #print(v)
                 v = torch.clip(v, -1, 1)
                 self.exp.dataset.set_vec(v_id, v.detach().cpu().numpy())
@@ -144,10 +148,11 @@ class Agent_NCA_genImage(Agent_NCA_gen):
         self.optimizer = optim.Adam(all_params, lr=self.exp.get_from_config('lr'), betas=self.exp.get_from_config('betas'))
 
         #self.optimizer = optim.Adam(self.model.parameters(), lr=self.exp.get_from_config('lr'), betas=self.exp.get_from_config('betas'))
-        self.optimizer_backpropTrick = optim.SGD(backdrop_trick_params, lr=16e-4)#, betas=self.exp.get_from_config('betas'))
+        #self.optimizer_backpropTrick = optim.Adam(backdrop_trick_params, lr=self.exp.get_from_config('lr'), betas=self.exp.get_from_config('betas')) #optim.SGD(backdrop_trick_params, lr=16e-4)#, betas=self.exp.get_from_config('betas'))
+        self.optimizer_backpropTrick = optim.SGD(backdrop_trick_params, lr=16e-2)
         #self.optimizer = optim.SGD(self.model.parameters(), lr=self.exp.get_from_config('lr'))
         self.scheduler = optim.lr_scheduler.ExponentialLR(self.optimizer, self.exp.get_from_config('lr_gamma'))
-        self.scheduler_backprop = optim.lr_scheduler.ExponentialLR(self.optimizer_backpropTrick, 0.9999)
+        self.scheduler_backprop = optim.lr_scheduler.ExponentialLR(self.optimizer_backpropTrick, 0.99995)
 
     @torch.no_grad()
     def test(self, *args, **kwargs):

src/models/DecoderOnly_Simple.py
@@ -2,6 +2,20 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels):
+        super(ResidualBlock, self).__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(in_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels, in_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(in_channels)
+        )
+
+    def forward(self, x):
+        return x + self.block(x)
+
 class DecoderOnly_Simple(nn.Module):
     def __init__(self, batch_size, extra_channels, device=torch.device("cuda:0"), hidden_size=512):
         super(DecoderOnly_Simple, self).__init__()
@@ -13,24 +27,29 @@ class DecoderOnly_Simple(nn.Module):
 
         self.drop0 = nn.Dropout(0.25)
 
+        # Sampling vector
+        self.fc3 = nn.Linear(self.extra_channels//2, 256*8*8)
+
         # Decoder
         self.decoder = nn.Sequential(
-            nn.Linear(extra_channels//2, 256 * 8 * 8),
-            nn.ReLU(),
             nn.Unflatten(1, (256, 8, 8)),
-            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),  # [batch, 128, 16, 16]
+            ResidualBlock(256),
+            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
-            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),  # [batch, 64, 32, 32]
+            ResidualBlock(128),
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
-            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),  # [batch, 32, 64, 64]
+            ResidualBlock(64),
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
-            nn.ConvTranspose2d(32, 3, kernel_size=4, stride=2, padding=1),  # [batch, 3, 128, 128]
-            nn.Sigmoid()
+            ResidualBlock(32),
+            nn.ConvTranspose2d(32, 3, kernel_size=4, stride=2, padding=1),
+            #nn.Sigmoid()
         )
 
         self.list_backpropTrick = []
         for i in range(batch_size):
-            self.list_backpropTrick.append(nn.Conv1d(extra_channels, extra_channels, kernel_size=1, stride=1, padding=0, groups=extra_channels))
+            self.list_backpropTrick.append(nn.Conv1d(extra_channels, extra_channels, kernel_size=1, stride=1, padding=0, groups=extra_channels).to(self.device))
 
     def reparameterize(self, mu, logvar):
         std = torch.exp(0.5 * logvar)
@@ -50,7 +69,8 @@ class DecoderOnly_Simple(nn.Module):
             emb = emb.to(self.device)[None, :]
         emb =  self.reparameterize(emb[:, :emb.shape[1]//2], emb[:, emb.shape[1]//2:])
 
-        x = self.decoder(emb).transpose(1,3)
+        x = self.fc3(emb)
+        x = self.decoder(x).transpose(1,3)
         
         #x = self.decoder(x).transpose(1,3)
         #x = x*2 -1