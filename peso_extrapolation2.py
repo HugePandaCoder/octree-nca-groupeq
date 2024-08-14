fix overflow loss

peso_extrapolation2.py
@@ -18,7 +18,7 @@ from src.utils.convert_to_cluster import convert_paths_to_cluster_paths
 print("Study Path:", ProjectConfiguration.STUDY_PATH)
 
 study_config = {
-        'experiment.name': r'peso_extrapolation_vitca_1_hidden_overflow_cluster',
+        'experiment.name': r'peso_extrapolation_vitca_2_fixed_overflow_cluster',
         'experiment.description': "OctreeNCAExtrapolation",
 }
 
@@ -33,6 +33,8 @@ study_config['experiment.logging.also_eval_on_train'] = False
 study_config['trainer.losses'] = ["torch.nn.L1Loss", "src.losses.OverflowLoss.OverflowLoss"]
 study_config['trainer.loss_weights'] = [1e2, 1e2]
 study_config['experiment.logging.evaluate_interval']= 2001
+
+study_config['performance.compile'] = True
 
 study_config = convert_paths_to_cluster_paths(study_config)
 

src/losses/OverflowLoss.py
@@ -16,12 +16,18 @@ class OverflowLoss(torch.nn.Module):
         loss_ret = {}
         loss = 0
 
-        hidden = kwargs['hidden_channels']
+        hidden: torch.Tensor = kwargs['hidden_channels']
 
         hidden_overflow_loss = (hidden - torch.clip(hidden, -1.0, 1.0)).abs().mean()
-        #rgb_overflow_loss = (output - torch.clip(output, 0, 1)).abs().mean()
+        with torch.no_grad():
+            _max = torch.amax(target, dim=tuple(range(1,len(target.shape))), keepdim=True)
+            _min = torch.amin(target, dim=tuple(range(1,len(target.shape))), keepdim=True)
 
-        loss = hidden_overflow_loss
+
+        rgb_overflow_loss = (output - torch.clamp(output, _min, _max)).abs().mean()
+
+        loss = hidden_overflow_loss + rgb_overflow_loss
         loss_ret['hidden'] = hidden_overflow_loss.item()
+        loss_ret['rgb'] = rgb_overflow_loss.item()
 
         return loss, loss_ret