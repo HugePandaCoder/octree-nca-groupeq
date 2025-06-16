58c344746e16b64e7f333092380c85ec7bb6ccab
Ahmed Chouchane <ahmedchouchane460@gmail.com>
2025-06-16 19:45:58 +0000

    test baseline model

--- src/utils/ProjectConfiguration.py
+++ src/utils/ProjectConfiguration.py
@@ -1,6 +1,6 @@
 import os
 class ProjectConfiguration:
-    STUDY_PATH = r"pvc_groupEq/groupeq_results/"
+    STUDY_PATH = r"pvc_groupEq/baseline_results/"
     VITB16_WEIGHTS = r"/home/jkalkhof_locale/Documents/GitHub/PretrainedVITs/imagenet21k_R50+ViT-B_16.npz"
     FILER_BASE_PATH = r"/local/scratch/"
 
